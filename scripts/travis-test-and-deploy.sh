8b068c9c19b2bb99dba71313648f7da0d3fe1f42
philipshurpik <philip.shurpik@gmail.com>
2016-11-04 22:06:03 +0200

    simplify test and deploy script

--- scripts/travis-test-and-deploy.sh
+++ scripts/travis-test-and-deploy.sh
@@ -11,7 +11,7 @@ echo message "$MESSAGE"
 echo '=================='
 
 
-if [[ $TRAVIS_BRANCH = release || $TRAVIS_BRANCH =~ ^ready\/.+ ]]; then
+if [[ $TRAVIS_BRANCH = master || $TRAVIS_BRANCH =~ ^ready\/.+ ]]; then
 	npm run eslint
 	defaults write com.apple.iphonesimulator ConnectHardwareKeyboard -bool NO
 	echo "Building native app"
@@ -19,26 +19,10 @@ if [[ $TRAVIS_BRANCH = release || $TRAVIS_BRANCH =~ ^ready\/.+ ]]; then
 	export DEVICE_NAME="iPhone 6s"
 	$(dirname $0)/./build-tests.sh > /dev/null
 	npm test
-fi
 
-if [[ $TRAVIS_BRANCH = master || $TRAVIS_BRANCH = release ]]; then
-	gem update match
-	gem update gym
-	gem update pilot
 	gem update fastlane
-	# Force bundler 1.12.5 because version 1.13 has issues, see https://github.com/fastlane/fastlane/issues/6065#issuecomment-246044617
-  	gem uninstall bundler -v '>1.12.5' --force || echo "bundler >1.12.5 is not installed"
-  	gem install bundler -v 1.12.5 --no-rdoc --no-ri --no-document --quiet
-
 	/usr/libexec/PlistBuddy -c "Set CFBundleVersion $TRAVIS_BUILD_NUMBER" ./ios/ReactNativeStarterKit/Info.plist
-
-	if [[ $TRAVIS_BRANCH = master ]]; then
-		npm run deploy-staging
-	fi
-
-	if [[ $TRAVIS_BRANCH = release ]]; then
-		npm run deploy-release
-	fi
+	npm run deploy-staging
 fi
 
 . scripts/merge-into-master.sh
